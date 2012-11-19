88ecb4cea2f32531c17426213e0d79d965d77be1
Aneesh Shreekant Mulye <aneesh.mulye@gmail.com>
2012-11-19 11:44:47 -0500

    Got rudimentary reads/writes working.

--- dft_graph.c
+++ dft_graph.c
@@ -7,11 +7,23 @@
 #define DFT_DEBUG 1
 #define STACK_SIZE (16*1024)
 #define TIMESLICE 10000
+#define BUFFER_SIZE 20
 
 typedef struct {
-	unsigned int **adjmat;
+	void *buf;
+	unsigned int size;
+	unsigned int head, tail;
+} dft_buffer;
+
+typedef struct {
+	dft_buffer ***adjmat;
 	unsigned int size;
 } dft_graph_t;
+
+static int dft_buffer_init(dft_buffer *, unsigned const int);
+static int dft_buffer_read(dft_buffer *, void *, unsigned const int);
+static int dft_buffer_write(dft_buffer *, void *, unsigned const int);
+static int dft_buffer_pretty(dft_buffer *);
 
 static dft_graph_t gg;
 static ucontext_t * contexts;
@@ -19,39 +31,50 @@ static ucontext_t user_context; /* TODO */
 static unsigned int current_thread_index;
 static unsigned int * thread_order;
 
+int dft_read(unsigned const int, void *, unsigned const int);
+int dft_write(unsigned const int, void *, unsigned const int);
+
 static int dft_graph_init(dft_graph_t * graph, unsigned const int size) {
 
-	int i;
+	int i, j;
 
 	if (!graph)
 		return -1;
 
-	graph->adjmat = malloc(size*sizeof(unsigned int *));
+	graph->adjmat = malloc(size*sizeof(dft_buffer **));
 	if (!graph->adjmat)
 		return -1;
 
 	for(i = 0; i < size; i++) {
-		graph->adjmat[i] = malloc(size*sizeof(unsigned int));
+		graph->adjmat[i] = malloc(size*sizeof(dft_buffer *));
 		if(!graph->adjmat[i]) {
 			if(DFT_DEBUG)
 				printf("DEBUG dft_graph_init : %s",
 					"adjacency matrix invalid\n");
 			return -1;
 		}
+		for(j = 0; j < size; j++)
+			graph->adjmat[i][j] = NULL;
 	}
 
 	graph->size = size;
 
 	return 0;
 } 
+
 static int dft_graph_link(dft_graph_t * graph, unsigned const int source,
 		unsigned const int dest) {
 	if(!graph)
 		return -1;
 	if(!graph->adjmat)
 		return -1;
-	graph->adjmat[source][dest] = 1;
-	return 0;
+
+	graph->adjmat[source][dest] = malloc(sizeof(dft_buffer));
+
+	if(!graph->adjmat[source][dest])
+		return -1;
+
+	return dft_buffer_init(graph->adjmat[source][dest], BUFFER_SIZE);
 }
 
 static int dft_graph_pretty(dft_graph_t * graph) {
@@ -67,7 +90,7 @@ static int dft_graph_pretty(dft_graph_t * graph) {
 	for(i=0; i<graph->size; i++) {
 		printf(">\t");
 		for(j=0; j<graph->size; j++) {
-			printf("%u ", graph->adjmat[i][j]);
+			printf("%u ", graph->adjmat[i][j] ? 1 : 0);
 		}
 		printf("\n");
 	}
@@ -208,4 +231,119 @@ int dft_thread_link(unsigned const int source, unsigned const int dest) {
 		return -1;
 
 	return dft_graph_link(&gg, source, dest);
+}
+
+int dft_read(unsigned const int in, void * readbuf, unsigned const int count) {
+
+	unsigned int i, done = -1;
+	unsigned int curr_thread = thread_order[current_thread_index];
+
+	/* DISABLE SIGNALS HERE */
+	for (i=0; i < gg.size; i++) {
+		if(gg.adjmat[i][curr_thread])
+			done++;
+		if(done == in)
+			break;
+	}
+
+	if(i == gg.size)
+		return -1;
+
+	done = dft_buffer_read(gg.adjmat[i][curr_thread], readbuf, count);
+	/* ENABLE SIGNALS HERE */
+	return done;
+}
+
+int dft_write(unsigned const int in, void * writebuf,
+		unsigned const int count) {
+	unsigned int i, done = -1;
+	unsigned int curr_thread = thread_order[current_thread_index];
+
+	/* DISABLE SIGNALS HERE */
+	for (i=0; i < gg.size; i++) {
+		if(gg.adjmat[curr_thread][i])
+			done++;
+		if(done == in)
+			break;
+	}
+
+	if(i == gg.size)
+		return -1;
+
+	done = dft_buffer_write(gg.adjmat[curr_thread][i], writebuf, count);
+	/* ENABLE SIGNALS HERE */
+	return done;
+}
+
+static int dft_buffer_init(dft_buffer * b, unsigned const int size) {
+
+	if (!b)
+		return -1;
+
+	b->buf = malloc(size+1);
+	if(!(b->buf))
+		return -1;
+
+	b->size = size+1;
+
+	b->head = 1;
+	b->tail = 0;
+
+	return 0;
+}
+
+static int dft_buffer_write(dft_buffer * b, void * writebuf, unsigned int count) {
+
+	unsigned int used;
+	unsigned int i;
+
+	if(!b || !b->buf || !writebuf)
+		return -1;
+
+	used = (b->size + b->head - b->tail - 1)%(b->size);
+
+	if ((used + count) > (b->size-1))
+		return -1;
+
+	for(i = 0; i < count; i++) {
+		*((char *)b->buf+b->head) = *((char *)writebuf + i);
+		b->head = (b->head+1)%(b->size);
+	}
+
+	return 0;
+}
+
+static int dft_buffer_read(dft_buffer * b, void * readbuf, unsigned int count) {
+
+	unsigned int used;
+	unsigned int i;
+
+	if(!b || !b->buf || !readbuf)
+		return -1;
+
+	used = (b->size + b->head - b->tail - 1)%(b->size);
+
+	if(count > used)
+		return -1;
+
+	for(i = 0; i < count; i++) {
+		*((char *)readbuf + i) =
+			*((char *)b->buf + (b->tail + 1)%(b->size));
+		b->tail = (b->tail+1)%(b->size);
+	}
+
+	return 0;
+}
+
+static int dft_buffer_pretty(dft_buffer * b) {
+
+	unsigned int i;
+
+	if(!b || !b->buf)
+		return -1;
+
+	for(i = b->tail + 1; i != b->head; i = (i+1)%(b->size))
+		printf("%c", *((char *)b->buf + i));
+
+	return 0;
 }
--- dft_graph_test.c
+++ dft_graph_test.c
@@ -4,14 +4,54 @@
 const int num_threads = 16;
 
 void foo(int);
-int main() {
+void source(int);
+void sink(int);
+void intermediate(int);
+
+int main_sched_test() {
 
 	int i;
 	unsigned int *order;
 
-	/* MARK: The following chunk of code is used to test the base graph
-	 * functions. None of the *_test functions should be interspersed with
-	 * the scheduler functions, as they use the same global graph (gg).
+	dft_init(num_threads);
+
+	for(i=0; i < num_threads; i++)
+		dft_thread_create(foo);
+
+	for(i = 0; i < (num_threads - 1); i++)
+		dft_thread_link(i+1, i);
+
+	dft_execute();
+
+	return 0;
+
+}
+
+int main_thread_test() {
+
+	int i;
+	unsigned int *order;
+
+	dft_init(num_threads);
+
+	dft_thread_create(source);
+	for(i=1; i < num_threads-1; i++)
+		dft_thread_create(intermediate);
+	dft_thread_create(sink);
+
+	for(i = 0; i < (num_threads - 1); i++)
+		dft_thread_link(i, i+1);
+
+	dft_execute();
+
+	return 0;
+
+}
+int main_graph_test() {
+
+	int i;
+	unsigned int *order;
+
 	dft_graph_init_test(16);
 	//for (i = 0; i < 100; i++)
 		//dft_graph_link(&g, i%15, i%13);
@@ -28,26 +68,57 @@ int main() {
 		printf("%d ", order[i]);
 
 	printf("\n");
-	*/
-
-	dft_init(num_threads);
-
-	for(i=0; i < num_threads; i++)
-		dft_thread_create(foo);
-
-	for(i = 0; i < (num_threads - 1); i++)
-		dft_thread_link(i+1, i);
-
-	dft_execute();
 
 	return 0;
+}
 
+int main() {
+
+	return main_thread_test();
 }
 
 void foo(int me)
 {
 	while(1) {
 		printf("I am thread %d\n", me);
+		sleep(1);
+		dft_yield();
+	}
+}
+
+void source(int me) {
+	char a;
+	unsigned int i = 0;
+	while(1) {
+		a = 'a' + (i%26);
+		i++;
+		printf("I am the source! All hail me!\n");
+		dft_write(0, &a, 1);
+		sleep(1);
+		dft_yield();
+	}
+}
+
+void intermediate(int me) {
+
+	char temp;
+
+	while(1) {
+		dft_read(0, &temp, 1);
+		dft_write(0, &temp, 1);
+		printf("I am thread %d, who just processed '%c'\n", me, temp);
+		sleep(1);
+		dft_yield();
+	}
+}
+
+void sink(int me) {
+
+	char b;
+
+	while(1) {
+		dft_read(0, &b, 1);
+		printf("I am the sink! I have consumed '%c'! Fear me!\n", b);
 		sleep(1);
 		dft_yield();
 	}
